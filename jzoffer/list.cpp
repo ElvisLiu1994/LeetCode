bc88a297ef2afb198b958ed48b4acce09ac45fbd
elvisliu <719880851@qq.com>
2018-05-08 23:55:02 +0800

    update

--- jzoffer/list.cpp
+++ jzoffer/list.cpp
@@ -137,6 +137,59 @@ ListNode* FindKthToTail(ListNode* pListHead, unsigned int k){
 // 题目：一个链表中包含环，如何找出环的入口结点？例如，在图3.8的链表中，
 // 环的入口结点是结点3。
 
+// 先找到两指针相遇的节点，如果不存在环，那么返回的将是nullptr
+ListNode* MeetingNode(ListNode* pHead){
+    if(pHead == nullptr)
+        return nullptr;
+
+    ListNode* pSlow = pHead->next;
+    if(pSlow == nullptr)
+        return nullptr;
+
+    ListNode* pFast = pSlow->next;
+    while(pSlow != nullptr && pFast != nullptr){
+        if(pFast == pSlow)
+            return pFast;
+
+        pSlow = pSlow->next;
+        pFast = pFast->next;
+        if(pFast != nullptr)
+            pFast = pFast->next;
+    }
+
+    return nullptr;
+}
+
+// 找到环中的一个节点之后，循环一圈就能知道环的长度，并能找到环的入口节点
+ListNode* EntryNodeOfLoop(ListNode* pHead){
+    ListNode* meetingNode = MeetingNode(pHead);
+    if(meetingNode == nullptr)
+        return nullptr;
+
+    //得到环中的节点数目
+    int nodesInLoop = 1;
+    ListNode* pNode1 = meetingNode;
+    while(pNode1->next != meetingNode){
+        nodesInLoop++;
+        pNode1 = pNode1->next;
+    }
+
+    //先移动pNode1，次数为环中节点的数目
+    pNode1 = pHead;
+    for(int i = 0; i < nodesInLoop; i++)
+        pNode1 = pNode1->next;
+
+    // 再移动pNode1和pNode2，则pNode1和pNode2最终同时指向入口节点
+    ListNode* pNode2 = pHead;
+    while(pNode1 != pNode2){
+        pNode1 = pNode1->next;
+        pNode2 = pNode2->next;
+    }
+
+    return pNode1;
+}
+
+/***************************************************************************/
 
 
 /***************************************************************************/
