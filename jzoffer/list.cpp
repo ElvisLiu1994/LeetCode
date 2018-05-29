f54023de527129641476e4a61b704f17ce7ab7a1
elvisliu <719880851@qq.com>
2018-05-29 23:30:33 +0800

    update

--- jzoffer/list.cpp
+++ jzoffer/list.cpp
@@ -191,6 +191,29 @@ ListNode* EntryNodeOfLoop(ListNode* pHead){
 
 /***************************************************************************/
 
+// 面试题24：反转链表
+// 题目：定义一个函数，输入一个链表的头结点，反转该链表并输出反转后链表的
+// 头结点。
+
+ListNode* ReverseList(ListNode* pHead){
+
+    ListNode* pReversedHead = nullptr;
+    ListNode* pNode = pHead;
+    ListNode* pPrev = nullptr;
+    while(pNode != nullptr){
+        ListNode* pNext = pNode->next;
+
+        if(pNext == nullptr)
+            pReversedHead = pNode;
+
+        pNode->next = pPrev;
+        pPrev = pNode;
+        pNode = pNext;
+    }
+
+    return pReversedHead;
+
+}
 
 /***************************************************************************/
 
